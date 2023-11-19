ad117fa893e1fc9e1adf1dbadccf2eac7b2ccd95
Botan Majeed <botan_art@hotmail.com>
2023-11-19 20:29:15 +0100

    Initial Commit

---
+++ WorkTimeTrackerPro/WorkTimeTrackerProApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkTimeTrackerProApp.swift
+//  WorkTimeTrackerPro
+//
+//  Created by Bootan Majeed on 2023-11-19.
+//
+
+import SwiftUI
+
+@main
+struct WorkTimeTrackerProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
